101811ce811f442e87b835a945e76d880596fe97
Ankita-Modak <ankitamodak15@gmail.com>
2023-04-21 10:20:23 +0530

    [Ankita]No Play

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -1,65 +1,55 @@
 ﻿using System;
 
-namespace SnakeLadder
+namespace SnakeLadder;
+class SnakeAndLadder
 {
-
-    class SnakeAndLadder
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int position = 0;
+        Console.WriteLine("Welcome to the Snake and Ladder game!");
+
+        while (position < 100)
         {
-            int position = 0;
-            Console.WriteLine("Welcome to the Snake and Ladder game!");
+            Console.WriteLine("Press any key to roll the dice");
+            Console.ReadKey(true);
 
-            while (position < 100)
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+
+            Console.WriteLine("You rolled a {0}", dice);
+
+            int option = random.Next(0, 3);
+            switch (option)
             {
-                Console.WriteLine("Press any key to roll the dice");
-                Console.ReadKey(true);
-
-                Random random = new Random();
-                int dice = random.Next(1, 7);
-
-                Console.WriteLine("You rolled a {0}", dice);
-
-                position += dice;
-
-                if (position > 100)
-                {
-                    position = 100 - (position - 100);
-                }
-
-                Console.WriteLine("You are now at position {0}", position);
-
-                switch (position)
-                {
-                    case 3:
-                        Console.WriteLine("You landed on a ladder! Moving up to position 21");
-                        position = 21;
-                        break;
-                    case 8:
-                        Console.WriteLine("You landed on a snake! Moving down to position 4");
-                        position = 4;
-                        break;
-                    case 28:
-                        Console.WriteLine("You landed on a ladder! Moving up to position 84");
-                        position = 84;
-                        break;
-                    case 58:
-                        Console.WriteLine("You landed on a snake! Moving down to position 36");
-                        position = 36;
-                        break;
-                    case 75:
-                        Console.WriteLine("You landed on a ladder! Moving up to position 91");
-                        position = 91;
-                        break;
-                    case 88:
-                        Console.WriteLine("You landed on a snake! Moving down to position 24");
-                        position = 24;
-                        break;
-                }
+                case 0:
+                    Console.WriteLine("No Play. You stay at position {0}", position);
+                    break;
+                case 1:
+                    position += dice;
+                    if (position > 100)
+                    {
+                        position = 100 - (position - 100);
+                    }
+                    Console.WriteLine("You landed on a ladder! Moving up to position {0}", position);
+                    break;
+                case 2:
+                    position -= dice;
+                    if (position < 0)
+                    {
+                        position = 0;
+                    }
+                    Console.WriteLine("You landed on a snake! Moving down to position {0}", position);
+                    break;
             }
 
-            Console.WriteLine("Congratulations, you won the game!");
-        }
-    }
+            if (position == 100)
+            {
+                break;
+            }
 
+            Console.WriteLine("You are now at position {0}", position);
+        }
+
+        Console.WriteLine("Congratulations, you won the game!");
+    }
 }
